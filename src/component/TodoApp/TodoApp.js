8ecf01b37ea2fae3dc8bb49e16dfd0f84e55dae0
Ahammed Hani <haniahammed7@gmail.com>
2024-10-16 19:58:33 +0530

    Implement add and delete functionalities

--- src/component/TodoApp/TodoApp.js
+++ src/component/TodoApp/TodoApp.js
@@ -2,18 +2,54 @@ import React, { Component } from 'react'
 import "./TodoApp.css"
 
 export default class TodoApp extends Component {
+  state = {
+    input: "",
+    items: []
+  }
+
+  handleChange = event => {
+    this.setState({
+      input: event.target.value
+    })
+  };
+
+  storeItems = event => {
+    event.preventDefault();
+    const { input } = this.state;
+    const allItems = this.state.items;
+
+    allItems.push(input);
+
+    this.setState({
+      items: allItems,
+      input: ""
+    })
+  }
+
+  deleteItem = key => {
+    const allItems = this.state.items;
+    allItems.splice(key, 1); 
+
+    this.setState({
+      items: allItems
+    })
+  }
+
   render() {
+    const { input,items } = this.state;
+
     return (
       <div className="todo-container">
-        
-        <form className='input-section'>
+        <form className='input-section' onSubmit={this.storeItems}>
             <h1>Todo App</h1>
-            <input type="text" placeholder='Enter Items..'/>
+            <input type="text" placeholder='Enter Items..' onChange={this.handleChange} value={input}/>
         </form>
         <ul>
-            <li>Items <i className="fas fa-trash-alt"></i></li>
-            <li>Items <i className="fas fa-trash-alt"></i></li>
-            <li>Items <i className="fas fa-trash-alt"></i></li>
+            {items.map((data,index) => (
+              <li key={index}>
+                {data} <i className="fas fa-trash-alt" onClick={()=>this.deleteItem(index)}></i>
+              </li>
+            ))}
         </ul>
       </div>
     )
